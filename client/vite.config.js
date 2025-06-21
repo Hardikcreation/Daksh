1963d97d7670d746d0a504ec425e3a69ae3e1b90
Hardik Creation <hardikprajapati3551@gmail.com>
2025-06-21 14:56:33 +0530

    config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          vendor: ['axios', 'lodash'] // add other common libs here
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000 // optional: suppress large chunk warning
+  }
 })
