d1bb33771465078ad0f64b41229fec91a0183198
HardikCreation <hardikprajapati3551@gmail.com>
2025-07-15 11:45:46 +0530

    card

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,7 @@ import App from './App';
 import './index.css';
 import { CartProvider } from './context/CartContext';
 import { AuthProvider } from './context/AuthContext';
+import './i18n';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
