376097515a1f6f3a46756fa3a443132c2bdbfec2
HardikCreation <hardikprajapati3551@gmail.com>
2025-07-29 13:00:46 +0530

    server

--- server/server.js
+++ server/server.js
@@ -36,7 +36,9 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'http://localhost:5175',
-  'https://daksh-client.onrender.com'
+  'https://daksh-admin.onrender.com',
+  'https://daksh-client.onrender.com',
+  'https://daksh-callcentre-panel.onrender.com',
 ];
 
 // ✅ CORS middleware
